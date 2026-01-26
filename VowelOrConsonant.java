ece8539b1054a9b6406c3ff21d4f9f561b061c5d
vaishnavic10 <vaishnavichopade316@gmail.com>
2026-01-26 20:20:31 +0530

    Initial commit -

---
+++ VowelOrConsonant.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+        
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+
+            ch = Character.toLowerCase(ch);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || 
+                ch == 'o' || ch == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+
+        } else {
+            System.out.println("Invalid Input");
+        }
+
+        sc.close();
+    }
+}
