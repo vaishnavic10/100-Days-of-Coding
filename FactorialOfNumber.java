3fe82748d163babd11e8a00b8f215b13445e7a4a
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-02-04 17:00:48 +0530

    Create FactorialOfNumber.java

---
+++ FactorialOfNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+  public static void main (String args[]) {
+    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+  int factorial = 1;
+    
+    for(int i = 1; i <= n; i++) {
+      factorial = factorial * 1;
+    }
+    
+    System.out.println(factorial);
+  }
+}
