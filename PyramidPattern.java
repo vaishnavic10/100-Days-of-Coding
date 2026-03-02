7e5751bc881156d46253c4b79c736db1612ba7d2
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-03-02 09:40:51 +0530

    print pyramid pattern

---
+++ PyramidPattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PyramidPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {      //spaces
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {      //stars
+                System.out.print("*");
+            }
+
+            System.out.println(); 
+        }
+
+        sc.close();
+    }
+}
