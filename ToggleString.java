859709a87df6e8a4826f0c318183a2c0b8a81140
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-03-09 20:11:08 +0530

    Implement ToggleString to toggle case of input

---
+++ ToggleString.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ToggleString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(ch >= 'A' && ch <= 'Z') {
+                result += (char)(ch + 32);  
+            }
+            else if(ch >= 'a' && ch <= 'z') {
+                result += (char)(ch - 32);  
+            }
+            else {
+                result += ch;
+            }
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+}
