1ec479d1bbada148f4e461711e40ed0911dd99e2
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-03-17 16:18:33 +0530

    Create ReverseString.java
    Fix variable declaration and add palindrome check.

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+  public class PalindromeString {
+    public static void main (String [] args) {
+      Scanner sc = new Scanner(System.in);
+      String str = sc.nextLine();
+      string reverse = "";
+
+    for(int i = str.length() - 1; i >= 0; i--) {
+      reverse = reverse + str.charAt(i);
+    }
+      if(str.equals(reverse))
+        System.out.println("Palindrome");
+      else
+        System.out.println("Not a Palindrome);
+    }
+  }
