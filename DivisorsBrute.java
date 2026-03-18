85f72f0d61dba83995683afcd384564492f96c75
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-03-18 13:02:07 +0530

    Update DivisorsBrute.java
    Optimize divisor finding loop to check up to the square root of n.

--- DivisorsBrute.java
+++ DivisorsBrute.java
@@ -6,11 +6,16 @@ public class DivisorsBrute {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 1; i <= n; i++)
+        for(int i = 1; i * i <= n; i++)
         {
             if(n % i == 0)
             {
                 System.out.print(i + " ");
+
+                if(i != n / i)
+                {
+                    System.out.print((n / i) + " ");
+                }
             }
         }
     }
