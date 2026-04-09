83c65a442981696830984a794d36409cd26392e2
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-04-09 11:22:22 +0530

    ..

---
+++ ReverseWords.java
@@ -0,0 +1,12 @@
+class Solution {
+  public String reverseWords (String s) {
+    String [] words = s.trim().split("\\s+");
+    StringBuilder result = new StringBuilder();
+
+  for (int i = words.length - 1; i >= 0; i--) {
+    
+    result.append(words[i]).append(" ");
+  }
+    return result.toString().trim();
+  }
+}
