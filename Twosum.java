66aa1674ea2eb4db3252ff2731b95c438fcd0f2e
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-04-16 11:05:31 +0530

    .

---
+++ Twosum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+public int[] twoSum(int [] nums, int target) {
+Hashmap <Integer, Integer> map = new Hahmap<>();
+
+for (int i = 0; i < nums.length; i++) {
+int complement = target - nums[i];
+
+if (map.containsKey(complement)){
+return new int[]{map.get(complement, i};
+}
+map.put (nums[i], i);
+  }
+return new int[]{};
+  }
+}
