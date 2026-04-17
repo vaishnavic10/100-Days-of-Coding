c73c2c8c8a6f6b505a344d93c0b67e4296589da9
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-04-17 15:32:18 +0530

    Implement majority element algorithm

---
+++ MajorityElement.java
@@ -0,0 +1,12 @@
+class Solutions {
+  public int majorityElements (int num []) {
+    int count = 0; int candidate = 0;
+    for (int num : nums) {
+      if (count == 0) candidate = num;
+
+      count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+  }
+} 
+  
